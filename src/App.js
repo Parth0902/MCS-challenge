5183d30c0fe2c57194f0a62dc4af02927332356e
Parth Godase <parthgodase123@gmail.com>
2023-10-16 01:30:03 +0530

    PAGE 1SECTION TWO DONE

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 import Footer from './components/Footer';
 import Hero from './components/Hero';
+import Section2 from './components/Section2';
 function App() {
   return (
     <div className="App">
         <Hero/>
+        <Section2/>
         <Footer/>
+      
     </div>
   );
 }
---
+++ src/components/Section2.jsx
@@ -0,0 +1,164 @@
+import React from 'react'
+
+const Section2 = () => {
+  return (
+    <div className='h-screen flex flex-col items-center gap-16'>
+        <div className='flex flex-col items-center pt-8 gap-8'>
+            <h1 className='text-6xl font-bold font-font1'>Want to <span style={{background: "linear-gradient(91deg, #0076CE 26.63%, #9400D3 65.81%)",backgroundClip: "text",webkitBackgroundClip: "text",WebkitTextFillColor: "transparent"}}>Join</span> Us?</h1>
+            <h4 className='font-font1 text-xl font-normal'>To remain with us, it is essential that you diligently follow the steps provided</h4>
+        </div>
+
+        <div className='flex flex-row mx-32 flex-wrap gap-10 justify-center '>
+            <div className='border border-gray-400 rounded-xl  px-5 ' style={{width:'405px'}} >
+                <div className=' relative bottom-4 right-8 w-8 h-8 rounded text-white flex flex-row justify-center items-center' style={{background:'var(--gradient-border-outline, linear-gradient(96deg, #0076CE 0%, #9400D3 100%))'}}>
+                     <span className='text white'> 1st</span>
+                </div>
+
+                <div className='flex flex-col gap-3 items-center pb-5'>
+                    <h4 className='text-center text-xl font-font1 font-bold'>Commencement of business </h4>
+                    <p className='text-center font-font1 font-normal text-base '>Invested shareholders must confirm payment and office address </p>
+
+                </div>
+
+                <div className='flex flex-row gap-16 justify-center pb-4'>
+                    <div className='flex flex-col gap-2'>
+                        <h4 style={{color:'#0076CE'}} className='font-font1 font-medium'>Due Dates</h4>
+                        <p>Within 180 days</p>
+                    </div>
+                    <div className='flex flex-col gap-4 items-center'>
+                         <h4  style={{color:'#FF6666'}} className='font-font1 font-medium'>Penalty fees</h4>
+                         <p ><span className='text-left font-font1 font-bold underline underline-offset-1'>₹50,000</span> for the company</p>
+                         <p ><span className='text-left font-font1 font-bold underline underline-offset-1'>₹1000</span>/day for directors</p>
+                    </div>
+                </div>
+
+            </div>
+            <div className='border border-gray-400 rounded-xl  px-5 ' style={{width:'405px'}} >
+                <div className=' relative bottom-4 right-8 w-8 h-8 rounded text-white flex flex-row justify-center items-center' style={{background:'var(--gradient-border-outline, linear-gradient(96deg, #0076CE 0%, #9400D3 100%))'}}>
+                     <span className='text white'> 1st</span>
+                </div>
+
+                <div className='flex flex-col gap-3 items-center pb-5'>
+                    <h4 className='text-center text-xl font-font1 font-bold'>Commencement of business </h4>
+                    <p className='text-center font-font1 font-normal text-base '>Invested shareholders must confirm payment and office address </p>
+
+                </div>
+
+                <div className='flex flex-row gap-16 justify-center pb-4'>
+                    <div className='flex flex-col gap-2'>
+                        <h4 style={{color:'#0076CE'}} className='font-font1 font-medium'>Due Dates</h4>
+                        <p>Within 180 days</p>
+                    </div>
+                    <div className='flex flex-col gap-4 items-center'>
+                         <h4  style={{color:'#FF6666'}} className='font-font1 font-medium'>Penalty fees</h4>
+                         <p ><span className='text-left font-font1 font-bold underline underline-offset-1'>₹50,000</span> for the company</p>
+                         <p ><span className='text-left font-font1 font-bold underline underline-offset-1'>₹1000</span>/day for directors</p>
+                    </div>
+                </div>
+
+            </div>
+            <div className='border border-gray-400 rounded-xl  px-5 ' style={{width:'405px'}} >
+                <div className=' relative bottom-4 right-8 w-8 h-8 rounded text-white flex flex-row justify-center items-center' style={{background:'var(--gradient-border-outline, linear-gradient(96deg, #0076CE 0%, #9400D3 100%))'}}>
+                     <span className='text white'> 1st</span>
+                </div>
+
+                <div className='flex flex-col gap-3 items-center pb-5'>
+                    <h4 className='text-center text-xl font-font1 font-bold'>Commencement of business </h4>
+                    <p className='text-center font-font1 font-normal text-base '>Invested shareholders must confirm payment and office address </p>
+
+                </div>
+
+                <div className='flex flex-row gap-16 justify-center pb-4'>
+                    <div className='flex flex-col gap-2'>
+                        <h4 style={{color:'#0076CE'}} className='font-font1 font-medium'>Due Dates</h4>
+                        <p>Within 180 days</p>
+                    </div>
+                    <div className='flex flex-col gap-4 items-center'>
+                         <h4  style={{color:'#FF6666'}} className='font-font1 font-medium'>Penalty fees</h4>
+                         <p ><span className='text-left font-font1 font-bold underline underline-offset-1'>₹50,000</span> for the company</p>
+                         <p ><span className='text-left font-font1 font-bold underline underline-offset-1'>₹1000</span>/day for directors</p>
+                    </div>
+                </div>
+
+            </div>
+            <div className='border border-gray-400 rounded-xl  px-5 ' style={{width:'405px'}} >
+                <div className=' relative bottom-4 right-8 w-8 h-8 rounded text-white flex flex-row justify-center items-center' style={{background:'var(--gradient-border-outline, linear-gradient(96deg, #0076CE 0%, #9400D3 100%))'}}>
+                     <span className='text white'> 1st</span>
+                </div>
+
+                <div className='flex flex-col gap-3 items-center pb-5'>
+                    <h4 className='text-center text-xl font-font1 font-bold'>Commencement of business </h4>
+                    <p className='text-center font-font1 font-normal text-base '>Invested shareholders must confirm payment and office address </p>
+
+                </div>
+
+                <div className='flex flex-row gap-16 justify-center pb-4'>
+                    <div className='flex flex-col gap-2'>
+                        <h4 style={{color:'#0076CE'}} className='font-font1 font-medium'>Due Dates</h4>
+                        <p>Within 180 days</p>
+                    </div>
+                    <div className='flex flex-col gap-4 items-center'>
+                         <h4  style={{color:'#FF6666'}} className='font-font1 font-medium'>Penalty fees</h4>
+                         <p ><span className='text-left font-font1 font-bold underline underline-offset-1'>₹50,000</span> for the company</p>
+                         <p ><span className='text-left font-font1 font-bold underline underline-offset-1'>₹1000</span>/day for directors</p>
+                    </div>
+                </div>
+
+            </div>
+            <div className='border border-gray-400 rounded-xl  px-5 ' style={{width:'405px'}} >
+                <div className=' relative bottom-4 right-8 w-8 h-8 rounded text-white flex flex-row justify-center items-center' style={{background:'var(--gradient-border-outline, linear-gradient(96deg, #0076CE 0%, #9400D3 100%))'}}>
+                     <span className='text white'> 1st</span>
+                </div>
+
+                <div className='flex flex-col gap-3 items-center pb-5'>
+                    <h4 className='text-center text-xl font-font1 font-bold'>Commencement of business </h4>
+                    <p className='text-center font-font1 font-normal text-base '>Invested shareholders must confirm payment and office address </p>
+
+                </div>
+
+                <div className='flex flex-row gap-16 justify-center pb-4'>
+                    <div className='flex flex-col gap-2'>
+                        <h4 style={{color:'#0076CE'}} className='font-font1 font-medium'>Due Dates</h4>
+                        <p>Within 180 days</p>
+                    </div>
+                    <div className='flex flex-col gap-4 items-center'>
+                         <h4  style={{color:'#FF6666'}} className='font-font1 font-medium'>Penalty fees</h4>
+                         <p ><span className='text-left font-font1 font-bold underline underline-offset-1'>₹50,000</span> for the company</p>
+                         <p ><span className='text-left font-font1 font-bold underline underline-offset-1'>₹1000</span>/day for directors</p>
+                    </div>
+                </div>
+
+            </div>
+            <div className='border border-gray-400 rounded-xl  px-5 ' style={{width:'405px'}} >
+                <div className=' relative bottom-4 right-8 w-8 h-8 rounded text-white flex flex-row justify-center items-center' style={{background:'var(--gradient-border-outline, linear-gradient(96deg, #0076CE 0%, #9400D3 100%))'}}>
+                     <span className='text white'> 1st</span>
+                </div>
+
+                <div className='flex flex-col gap-3 items-center pb-5'>
+                    <h4 className='text-center text-xl font-font1 font-bold'>Commencement of business </h4>
+                    <p className='text-center font-font1 font-normal text-base '>Invested shareholders must confirm payment and office address </p>
+
+                </div>
+
+                <div className='flex flex-row gap-16 justify-center pb-4'>
+                    <div className='flex flex-col gap-2'>
+                        <h4 style={{color:'#0076CE'}} className='font-font1 font-medium'>Due Dates</h4>
+                        <p>Within 180 days</p>
+                    </div>
+                    <div className='flex flex-col gap-4 items-center'>
+                         <h4  style={{color:'#FF6666'}} className='font-font1 font-medium'>Penalty fees</h4>
+                         <p ><span className='text-left font-font1 font-bold underline underline-offset-1'>₹50,000</span> for the company</p>
+                         <p ><span className='text-left font-font1 font-bold underline underline-offset-1'>₹1000</span>/day for directors</p>
+                    </div>
+                </div>
+
+            </div>
+
+          <p className='font-medium font-font1 px-28'>* For forms AOC-4 and MGT-7, you will be charged a penalty of ₹200 <span className='font-bold text-lg'>every day</span>until you file the form . There is no maximum penalty amount. So, if you don't file the form for a year, you will owe ₹73,000 per form</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Section2
